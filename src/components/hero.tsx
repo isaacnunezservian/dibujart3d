f5f6953631c828f70a4a4189fa6885aae29ca1e0
isaacnunezservian <isaac.nunezservian@gmail.com>
2025-08-08 12:20:42 -0300

    Comment Skeleton and fix typescript errors

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -66,9 +66,9 @@ Cada producto con sus caracteristicas      </p>
 );
 }
 
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-);
+// const Skeleton = () => (
+//   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+// );
 
 const items = [
   {
